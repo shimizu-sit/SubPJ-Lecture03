4a068e480b64b08b22dfe412b1b2d042627502e1
Shimizu Tetsuya <shimizu@info.shonan-it.ac.jp>
2021-07-25 17:00:31 +0900

    Finished chapter 2.

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -8,8 +8,8 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
-    [SerializeField, Range(0, 2)]
-    int function;
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
 
     Transform[] points;
 
@@ -33,23 +33,14 @@ public class Graph : MonoBehaviour
 
     void Update()
     {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+
         float time = Time.time;
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            if (function == 0)
-            {
-                position.y = FunctionLibrary.Wave(position.x, time);
-            }
-            else if (function == 1)
-            {
-                position.y = FunctionLibrary.MultiWave(position.x, time);
-            }
-            else
-            {
-                position.y = FunctionLibrary.Ripple(position.x, time);
-            }
+            position.y = f(position.x, time);
             point.localPosition = position;
         }
     }
